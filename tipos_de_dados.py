d8372095672c7f664c93f46875f03770f4eaa226
Gabriel Mendes <gabrielomendes02@gmail.com>
2024-04-30 14:22:01 -0300

    Aprendendo sobre os tipos de dados

---
+++ tipos_de_dados.py
@@ -0,0 +1,15 @@
+# Listas
+Lista_Exemplo_01 = [1, 2, 3, 4, 5]
+Lista_Exemplo_02 = ["Gabriel", "João", "Maria", "José"]
+
+# Tuplas -- São imutáveis
+Tupla_Exemplo_01 = (1, 2, 3, 4, 5)
+Tupla_Exemplo_02 = ("Gabriel", "João", "Maria", "José")
+
+# Dicionários
+dicionario = {
+    "index": 1,
+    idade: 22,
+    altura: 1.80,
+    "nome": "Gabriel"
+}
